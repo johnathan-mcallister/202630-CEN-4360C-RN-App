8c1a0d2c85c27c188bb11fb03792705106716b64
Johnathan McAllister <doorman.jm@gmail.com>
2026-07-10 11:44:18 -1200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
-const typescriptConfig = require("@typescript-eslint/eslint-plugin");
+const typescriptConfig = require("eslint-plugin");
 
 module.exports = defineConfig([
   expoConfig,
